63fa2b1c956e75137d293d8f834ee863e0732952
chiraggarg025 <chiraggarg025@gmail.com>
2020-09-09 18:00:34 +0530

    subscribe to the sore

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import {data} from '../data';
+// import {data} from '../data';
 import Navbar from './Navbar'
 import MovieCard from './MovieCard'
-function App() {
+import { data } from '../data';
+class App extends React.Component {
+  componentDidMount(){
+    // make an api call
+    // dspatch action
+    const {store} =this.props;
+    store.subscribe(()=>{
+      console.log('UPDATED');
+      // we should avoid this method
+      this.forceUpdate();
+    })
+    this.props.store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+    });
+    console.log('state',this.props.store.getState())
+  }
+  render(){
+    const movies = this.props.store.getState();
+    console.log('RENDER');
   return (
     <div className="App">
       <Navbar />
@@ -14,13 +33,15 @@ function App() {
       
 
         <div className="list">
-          {data.map((movie,index) =>(
+          {movies.map((movie,index) =>(
             <MovieCard movie={movie} key={`movies - ${index}`} />
           ))}
         </div>
       </div>
     </div>
   );
+  }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import './index.css';
 import App from './components/App';
 import movies from './reducers';
 const store  = createStore(movies)
-console.log('store',store);
-console.log('before state',store.getState());
+// console.log('store',store);
+// console.log('before state',store.getState());
 
-store.dispatch({
-  type:'ADD_MOVIES',
-  movies:[{name:'Superman'}]
-})
-console.log('after state',store.getState());
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'Superman'}]
+// })
+// console.log('after state',store.getState());
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
